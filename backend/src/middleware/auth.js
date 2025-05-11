e106ede0c134022f92cbb769e44c819eb1c5f33d
ali adham <thehillofsunflowers@gmail.com>
2025-05-11 15:34:51 -0700

    Fix middleware to allow for comment posts

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,4 +1,22 @@
 const { authenticateToken } = require('./authMiddleware');
 
-// Export the authenticateToken function as the default middleware
-module.exports = authenticateToken; 
+// Create a wrapper around authenticateToken that sets req.user instead of req.userId
+const authWithUserObj = (req, res, next) => {
+  // Call the original authenticateToken middleware
+  authenticateToken(req, res, (err) => {
+    if (err) {
+      return next(err);
+    }
+    
+    // If authenticateToken succeeded, it will have set req.userId
+    // Now transform that into req.user for the commentController
+    if (req.userId) {
+      req.user = { id: req.userId };
+    }
+    
+    next();
+  });
+};
+
+// Export the wrapped middleware
+module.exports = authWithUserObj; 
